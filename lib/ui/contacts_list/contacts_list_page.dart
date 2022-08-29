66f75a22b3cecd3876f1e0fb940db43ba4f965b0
Simon Koerner <simon.koerner@nexenio.com>
2022-08-29 19:00:26 +0200

    Use faker lib to generate fake contacts

--- lib/ui/contacts_list/contacts_list_page.dart
+++ lib/ui/contacts_list/contacts_list_page.dart
@@ -1,21 +1,18 @@
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 
 class ContactsListPage extends StatelessWidget {
-  final List<String> items =
-      List<String>.generate(10000, (i) => 'Contact number $i');
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Contacts')),
       body: ListView.builder(
-        itemCount: items.length,
-        prototypeItem: ListTile(
-          title: Text(items.first),
-        ),
+        itemCount: 30,
         itemBuilder: (context, index) {
           return ListTile(
-            title: Center(child: Text(items[index])),
+            title: Center(
+                child: Text(
+                    '${faker.person.firstName()} ${faker.person.lastName()}')),
           );
         },
       ),
