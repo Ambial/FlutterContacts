128aca49acf2c4dda5db54ccd3082a3043314525
Simon Koerner <simon.koerner@nexenio.com>
2022-08-29 18:42:08 +0200

    Generate long list with stupid data

--- lib/ui/contacts_list/contacts_list_page.dart
+++ lib/ui/contacts_list/contacts_list_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
 class ContactsListPage extends StatelessWidget {
+  final List<String> items =
+      List<String>.generate(10000, (i) => 'Contact number $i');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Contacts')),
+      body: ListView.builder(
+        itemCount: items.length,
+        prototypeItem: ListTile(
+          title: Text(items.first),
+        ),
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Center(child: Text(items[index])),
+          );
+        },
+      ),
     );
   }
 }
