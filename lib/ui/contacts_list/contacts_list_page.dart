896699a05a9761de7b02022320558d2428d819c3
Simon Koerner <simon.koerner@nexenio.com>
2022-08-31 17:42:50 +0200

    StatefulWidget, use initState, fix phoneNo

--- lib/ui/contacts_list/contacts_list_page.dart
+++ lib/ui/contacts_list/contacts_list_page.dart
@@ -1,18 +1,37 @@
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_contacts/data/contact.dart';
 
-class ContactsListPage extends StatelessWidget {
+class ContactsListPage extends StatefulWidget {
+  @override
+  State<ContactsListPage> createState() => _ContactsListPageState();
+}
+
+class _ContactsListPageState extends State<ContactsListPage> {
+  late List<Contact> _contacts;
+
+  // Runs when the widget is being inserted into the widget tree
+  @override
+  void initState() {
+    super.initState();
+    _contacts = List.generate(
+        50,
+        (index) => Contact(
+            name: '${faker.person.firstName()} ${faker.person.lastName()}',
+            email: faker.internet.email(),
+            phoneNumber: faker.phoneNumber.us()));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Contacts')),
       body: ListView.builder(
-        itemCount: 30,
+        itemCount: _contacts.length,
         itemBuilder: (context, index) {
           return ListTile(
-            title: Center(
-                child: Text(
-                    '${faker.person.firstName()} ${faker.person.lastName()}')),
+            title: Center(child: Text(_contacts[index].name)),
+            subtitle: Center(child: Text(_contacts[index].phoneNumber)),
           );
         },
       ),
